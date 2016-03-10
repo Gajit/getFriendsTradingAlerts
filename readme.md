850a0eff4145ac92eac19b96480bd5159ec9f4e1
Greg Johns <johns670@avaya.com>
2016-03-10 08:25:24 -0600

    source code for GFTA module

---
+++ readme.md
@@ -0,0 +1,13 @@
+# getFriendsTradingAlerts
+
+Alerts for friends weekly stock trading activities.
+
+## Dependencies
+- getFriendsListForUser
+- getTradeTransactionsForUser
+
+## Usage
+
+
+## API
+getFriendsTradingAlerts(userId)
---
+++ src/index.js
@@ -0,0 +1,69 @@
+function getFriendsTradingAlerts(uid) {
+  const friends = getFriendsListForUser(uid);
+  const activity = [];
+  const ordered = [];
+  const alerts = [];
+
+  // project all friends transactions into the activity array
+  friends.forEach( friend => {
+    const activityStrings = getTradeTransactionsForUser(friend);
+    activityStrings.forEach( el => activity.push(el.split(',').splice(1)));
+  });
+
+  // tease out activity noise to individual threads
+  let threads = activity
+      .reduce((symbols, arr, curIdx, orgArr) => {
+        let count = 0;
+        symbols[arr[1]] = [];
+        return symbols;
+      }, {});
+
+  // score ticker threads
+  activity.forEach( a => {
+    let action = a[0];
+     if ( action === "BUY") {
+       count = 1;
+     } else if ( action === "SELL") {
+       count = -1;
+     }
+     threads[a[1]].push(count);
+  });
+
+  // create and order alert objects in ordered array
+  function orderUp (net_friends, transaction, ticker){
+    ordered.push({
+      'net_friends': net_friends,
+      'buy_sell': transaction,
+      'ticker': ticker
+    });
+    ordered.sort((a, b) => {
+      return parseFloat(a.net_friends) - parseFloat(b.net_friends);
+    });
+  };
+
+  // gather ticker scores
+  for (var ticker in threads) {
+    let net_friends = 0;
+    let transaction = null;
+
+    // total ticker scores
+    threads[ticker].forEach( tick => {
+      net_friends += tick;
+    });
+
+    // set net trasaction for the ticker
+    if (net_friends > 0){
+      transaction = 'BUY';
+      orderUp(net_friends, transaction, ticker);
+    } else if (net_friends < 0) {
+      net_friends *= -1; // convert to pos num
+      transaction = 'SELL';
+      orderUp(net_friends, transaction, ticker);
+    }
+  }
+
+  // use the ordered alert object to push values into alert array
+  ordered.forEach(e => alerts.push(e.net_friends +','+ e.buy_sell +','+ e.ticker));
+
+  return alerts.reverse();
+}
