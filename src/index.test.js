c109b6067a99d2e488c81f3442fcf7fa18ad46e6
Greg Johns <johns670@avaya.com>
2016-03-12 13:34:53 -0600

    added some tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,26 @@
 const expect = require('chai').expect;
 const alerts = require('./index');
+const results = alerts('f1');
 
-describe('alerts', function() {
-  it('should work.', function() {
-    expect(true).to.be.true;
-  })
+describe('alerts', () => {
+  describe('getFriendsTradingAlerts', () => {
+    it('should return an array', () => {
+      expect(results).to.be.a('array')
+    });
+
+    it('should be an array of strings', () => {
+      expect(results).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every((item) => {
+          return typeof item === 'string';
+        });
+      }
+    });
+
+    it('should be in csv format', () => {
+      expect(results[0]).to.include(',')
+    });
+
+  });
 });
